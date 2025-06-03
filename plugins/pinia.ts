8a729c31a54486a03b0fdc7bd848a265619add78
Gaëtan Perrier <115650976+GaetanPERRIER@users.noreply.github.com>
2025-06-03 18:50:18 +0000

    fix

--- plugins/pinia.ts
+++
@@ -1,9 +0,0 @@
-import { useMainStore } from '~/store/index.js'
-
-export default defineNuxtPlugin(({ $pinia }) => {
-    return {
-        provide: {
-            store: useMainStore(),
-        }
-    }
-})
